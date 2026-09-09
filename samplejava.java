54c4a3a453aab434865d0481597b09a5c24ab4a7
RamyasriTejasri <ramya24d21a05h8@gmail.com>
2026-09-09 17:58:00 +0530

    Fix syntax error in print statement

--- samplejava.java
+++ samplejava.java
@@ -1,6 +1,6 @@
 public class samplejava{
     public static void main(String[]args){
         String name="Hello Ramya";
-        system.out.println("Hello,my name is"+name)
+        system.out.println("Hello,my name is"+name);
     }
 }
